d13ba189e0bd7fc282c205d1c7b873c7037f2338
KongzuK <130357707+KongzuK@users.noreply.github.com>
2025-03-15 18:49:48 +0000

    家具选择交互版本2

--- Assets/Scripts/ARObjectMove.cs
+++ Assets/Scripts/ARObjectMove.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+/*using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
@@ -43,6 +43,79 @@ public class ARObjectMove : MonoBehaviour
         else
         {
             selectedObject = null;
+        }
+    }
+}
+*/
+
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using System.Collections.Generic;
+
+public class ARObjectMove : MonoBehaviour
+{
+    private ARRaycastManager raycastManager;
+    private Vector2 touchPosition;
+    private GameObject selectedObject;
+    private Camera arCamera;
+    private bool isDragging = false;
+
+    void Start()
+    {
+        raycastManager = FindObjectOfType<ARRaycastManager>();
+        arCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.touchCount == 1)
+        {
+            Touch touch = Input.GetTouch(0);
+            touchPosition = touch.position;
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                TrySelectObject(touchPosition);
+            }
+            else if (touch.phase == TouchPhase.Moved && isDragging)
+            {
+                MoveSelectedObject(touchPosition);
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                isDragging = false;
+                selectedObject = null;
+            }
+        }
+    }
+
+    // 选中可拖动的家具
+    private void TrySelectObject(Vector2 screenPosition)
+    {
+        Ray ray = arCamera.ScreenPointToRay(screenPosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.CompareTag("Furniture"))
+            {
+                selectedObject = hit.transform.gameObject;
+                isDragging = true;
+            }
+        }
+    }
+
+    // 移动家具位置
+    private void MoveSelectedObject(Vector2 screenPosition)
+    {
+        if (selectedObject == null) return;
+
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        if (raycastManager.Raycast(screenPosition, hits, TrackableType.Planes))
+        {
+            Pose hitPose = hits[0].pose;
+            selectedObject.transform.position = hitPose.position;
         }
     }
 }
